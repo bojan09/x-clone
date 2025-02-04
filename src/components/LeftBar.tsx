5b67ef61872fe738eb828a6bd55d1a5ec203c7df
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2025-02-04 20:33:37 +0100

    minor style bug fixes

--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -1,3 +1,100 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const menuList = [
+  {
+    id: 1,
+    name: "Homepage",
+    link: "/",
+    icon: "home.svg",
+  },
+  {
+    id: 2,
+    name: "Explore",
+    link: "/",
+    icon: "explore.svg",
+  },
+  {
+    id: 3,
+    name: "Notification",
+    link: "/",
+    icon: "notification.svg",
+  },
+  {
+    id: 4,
+    name: "Messages",
+    link: "/",
+    icon: "message.svg",
+  },
+  {
+    id: 5,
+    name: "Bookmarks",
+    link: "/",
+    icon: "bookmark.svg",
+  },
+  {
+    id: 6,
+    name: "Jobs",
+    link: "/",
+    icon: "job.svg",
+  },
+  {
+    id: 7,
+    name: "Communities",
+    link: "/",
+    icon: "community.svg",
+  },
+  {
+    id: 8,
+    name: "Premium",
+    link: "/",
+    icon: "logo.svg",
+  },
+  {
+    id: 9,
+    name: "Profile",
+    link: "/",
+    icon: "profile.svg",
+  },
+  {
+    id: 10,
+    name: "More",
+    link: "/",
+    icon: "more.svg",
+  },
+];
+
 export default function LeftBar() {
-  return <div>Left Bar</div>;
+  return (
+    <div className="h-screen sticky top-0 flex flex-col justify-between pt-2 pb-8">
+      {/* Logo Button Menu */}
+      <div className="h-screen sticky top-0 flex flex-col justify-between pt-2 pb-8">
+        {/* Logo */}
+        <Link href="/" className="p-2 rounded-full hover:bg-[#181818]">
+          <Image src="/icons/logo.svg" alt="logo" width={24} height={24} />
+        </Link>
+
+        {/* Menu List */}
+        <div className="flex flex-col gap-4">
+          {menuList.map((item) => (
+            <Link
+              href={item.link}
+              className="p-2 rounded-full hover:bg-[#181818] flex items-center gap-4"
+              key={item.id}
+            >
+              <Image
+                src={`icons/${item.icon}`}
+                alt={item.name}
+                width={24}
+                height={24}
+              />
+              <span className="hidden lg:inline">{item.name}</span>
+            </Link>
+          ))}
+        </div>
+      </div>
+      {/* User */}
+      <div className="">User</div>
+    </div>
+  );
 }
