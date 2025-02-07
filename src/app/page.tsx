32bce278b43a5f8fe9371d2f4dff7f997c274595
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2025-02-07 08:55:48 +0100

    added Post, Post Info, Post Interactions. Work in progress on Share comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,38 @@
-import Image from "@/components/Image";
+import Feed from "@/components/Feed";
+import Share from "@/components/Share";
+import Post from "@/components/Post";
+
+import Link from "next/link";
 
 const Homepage = () => {
   return (
     <div className="">
       Homepage
-      <Image path="/general/post.jpeg" alt="image" width={600} height={600} />
+      <div className="p-4 flex justify-between text-textGray font-bold border-b-[1px] border-borderGray">
+        <Link
+          className="pb-3 flex items-center border-b-4 border-iconBlue "
+          href="/"
+        >
+          For you
+        </Link>
+
+        <Link className="pb-3 flex items-center" href="/">
+          Following
+        </Link>
+
+        <Link className="pb-3 flex items-center" href="/">
+          Next.js
+        </Link>
+        <Link className="pb-3 flex items-center" href="/">
+          React.js
+        </Link>
+        <Link className="pb-3 flex items-center" href="/">
+          JavaScript
+        </Link>
+      </div>
+      <Share />
+      <Post />
+      <Feed />
     </div>
   );
 };
---
+++ src/components/Post.tsx
@@ -0,0 +1,55 @@
+import Image from "@/components/Image";
+import PostInfo from "@/components/PostInfo";
+import PostInteractions from "@/components/PostInteractions";
+
+const Post = () => {
+  return (
+    <div className="p-4 border-y-[1px] border-borderGray">
+      {/* Post type */}
+      <div className="flex items-center gap-2 text-sm text-textGray mb-2 font-bold">
+        icon
+        <span>Batman reposted</span>
+      </div>
+      {/* Post content */}
+      <div className="flex gap-4">
+        {/* Avatar */}
+        <div className="relative w-10 h-10 rounded-full overflow-hidden">
+          <Image
+            path="general/avatar.jpg"
+            alt="post_image"
+            width={100}
+            height={100}
+            transformation={true}
+          />
+        </div>
+        {/* Content */}
+        <div className="flex-1 flex-col gap-2">
+          {/* Top Section */}
+          <div className="flex items-center justify-between gap-2">
+            <div className="flex items-center gap-2 flex-wrap">
+              <h1 className="text-md font-bold">Batman</h1>
+              <span className="text-textGray">@notbatman</span>
+              <span className="text-textGray">1 hour ago </span>
+            </div>
+            <PostInfo />
+          </div>
+          {/* Text & Media */}
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+            doloribus rerum eos, sint ab repellat excepturi dicta nobis non
+            corporis deserunt facilis voluptatem temporibus? Vitae!
+          </p>
+          <Image
+            path="general/post.jpeg"
+            alt="post_image"
+            width={600}
+            height={600}
+          />
+          <PostInteractions />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
